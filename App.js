19dae50f79a5005d70b2f5e2f683890ba04edf89
Havindu Piyumal Dissanayake <havindu1992@gmail.com>
2023-05-31 07:11:35 +1000

    Initial App layout with andriod status bar support

--- App.js
+++ App.js
@@ -1,20 +1,37 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar as ExpoStatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, SafeAreaView, Platform, StatusBar} from 'react-native';
+
+const isAndriod = Platform.OS === 'andriod';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Our react native blank canvas</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <SafeAreaView style={styles.container}>
+        <View style={styles.searchBar} >
+          <Text>Search</Text>
+        </View>
+        <View style={styles.listView} >
+          <Text>List</Text>
+        </View>
+        <StatusBar style="auto" />
+      </SafeAreaView>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    paddingTop: isAndriod ? StatusBar.currentHeight : 0,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
+  searchBar: {
+    backgroundColor: 'green',
+    padding: 16,
+  },
+  listView: {
+    flex: 1,
+    backgroundColor: 'blue',
+    padding: 16,
+  }
 });
