c958bf68b3215c11c82c7a43fb73c41b05297d70
Havindu Piyumal Dissanayake <havindu1992@gmail.com>
2023-06-03 13:31:25 +1000

    expo font setup

--- App.js
+++ App.js
@@ -1,11 +1,29 @@
 import { StatusBar as ExpoStatusBar } from "expo-status-bar";
 
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
 import { RestaurantScreen } from "./src/features/restaurants/screens/restaurants.screen";
 import { ThemeProvider } from "styled-components/native";
 
 import { theme } from "./src/infrastructure/theme";
 
 export default function App() {
+  const [oswaldFontsLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoFontsLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldFontsLoaded || !latoFontsLoaded) {
+    return null;
+  }
+
   return (
     <>
       <ThemeProvider theme={theme}>
