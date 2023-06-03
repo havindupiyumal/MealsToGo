04e65b86e84d4275d164f4235d2622234b6e2feb
Havindu Piyumal Dissanayake <havindu1992@gmail.com>
2023-06-03 21:16:57 +1000

    flat list contentcontainerstyle styles using styled components

--- src/features/restaurants/screens/restaurants.screen.styles.js
+++ src/features/restaurants/screens/restaurants.screen.styles.js
@@ -12,6 +12,8 @@ export const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
 `;
 
-export const RestaurantList = styled(FlatList)`
-  padding: ${(props) => props.theme.space[3]};
-`;
+export const RestaurantList = styled(FlatList).attrs({
+  contentContainerStyle: {
+    padding: 16,
+  },
+})``;
